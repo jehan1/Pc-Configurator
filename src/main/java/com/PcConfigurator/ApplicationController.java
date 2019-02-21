c0a760776ceac87a0e17efa749d11d225b313b64
jehan <jehan.nilash@hotmail.com>
2019-02-21 12:13:40 +0000

    Entity intial commit

--- src/main/java/com/PcConfigurator/ApplicationController.java
+++ src/main/java/com/PcConfigurator/ApplicationController.java
@@ -7,6 +7,7 @@ package com.PcConfigurator;
 
 import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
@@ -18,44 +19,55 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ApplicationController {
     
-    ProcessorMock processorMock = ProcessorMock.getInstance();
+    @Autowired
+    ProcessorRepo processorRespository;
 
-    @GetMapping("/Processor")
-    public List<Processor> index(){
-        return processorMock.fetchProcessors();
+    @GetMapping(path="/Processor")
+    public @ResponseBody Iterable<Processor> index(){
+        return processorRespository.findAll();
     }
-
-    @GetMapping("/Processor/{id}")
-    public Processor show(@PathVariable String id){
-        int ProcessorId = Integer.parseInt(id);
-        return processorMock.getProcessorById(ProcessorId);
-    }
-
-    @PostMapping("/Processor/search")
+    
+    @PostMapping(path="/Processor/Search")
     public List<Processor> search(@RequestBody Map<String, String> body){
         String searchTerm = body.get("text");
-        return processorMock.searchProcessors(searchTerm);
+            return processorRespository.findByNameContaining(searchTerm);
     }
 
-    @PostMapping("/Processor")
-    public Processor create(@RequestBody Map<String, String> body){
-        int id = Integer.parseInt(body.get("id"));
-        String title = body.get("title");
-        String content = body.get("content");
-        return processorMock.createProcessor(id, title, content);
-    }
-
-    @PutMapping("/Processor/{id}")
-    public Processor update(@PathVariable String id, @RequestBody Map<String, String> body){
-        int ProcessorId = Integer.parseInt(id);
-        String title = body.get("title");
-        String content = body.get("content");
-        return processorMock.updateProcessor(ProcessorId, title, content);
-    }
-
-    @DeleteMapping("Processor/{id}")
-    public boolean delete(@PathVariable String id){
-        int ProcessorId = Integer.parseInt(id);
-        return processorMock.delete(ProcessorId);
-    }
+//    @GetMapping("/Processor/{id}")
+//    public Processor show(@PathVariable String id){
+//        int ProcessorId = Integer.parseInt(id);
+//        return processorRespository.getOne(ProcessorId);
+//    }
+//
+//   // @PostMapping("/Processor/search")
+//   // public List<Processor> search(@RequestBody Map<String, String> body){
+//    //    String searchTerm = body.get("text");
+//     //   return processorRespository.findByTitleContainingOrContentContaining
+//     //   (searchTerm, searchTerm, searchTerm);
+//    //}
+//
+//    @PostMapping("/Processor")
+//    public Processor create(@RequestBody Map<String, String> body){
+//        int id = Integer.parseInt(body.get("id"));
+//        String name = body.get("name");
+//        String content = body.get("content");
+//        return processorRespository.save(new Processor());
+//    }
+//
+////    @PutMapping("/Processor/{id}")
+////    public Processor update(@PathVariable String id, @RequestBody Map<String, String> body){
+////        int ProcessorId = Integer.parseInt(id);
+////        Processor pro = processorRespository.findOne(ProcessorId);   
+////        String title = body.get("title");
+////        String content = body.get("content");
+////        return processorRespository.save(pro);
+////    }
+////
+////    @DeleteMapping("Processor/{id}")
+////    public boolean delete(@PathVariable String id){
+////        int ProcessorId = Integer.parseInt(id);
+////        processorRespository.delete(ProcessorId);
+////        return true;
+////        
+////    }
 }
--- src/main/java/com/PcConfigurator/Processor.java
+++ src/main/java/com/PcConfigurator/Processor.java
@@ -5,54 +5,50 @@
  */
 package com.PcConfigurator;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  *
- * @author Jehan & Chethika
+ * @author Jehan 
  */
-public class Processor {
-    private int id;
-    private String title;
-    private String content;
-
-    public Processor() {  }
-
-    public Processor(int id, String title, String content) {
-        this.setId(id);
-        this.setTitle(title);
-        this.setContent(content);
+@Entity
+@Table(name="processor")
+public class Processor extends Parts  {
+   
+    private String speed;
+    private String socket;
+    
+    public Processor(){}
+    
+    public Processor(int id, String name, String speed, String socket ) {
+        super(id, name);
+        this.setSpeed(speed);
+        this.setSocket(socket);
+    }
+  
+    public String getSpeed() {
+        return speed;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Processor{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
+    public void setSpeed(String speed) {
+        this.speed = speed;
     }
     
+    public String getSocket() {
+        return socket;
+    }
+
+    public void setSocket(String socket) {
+        this.socket = socket;
+    }
+
+    //@Override
+   /* public String toString() {
+        return "Processor{" +
+                "id=" + id +
+                ", title='" + name + '\'' +
+                ", Speed='" + speed + '\'' + ", Socket='" + socket + '\'' +
+                '}';
+    }*/
 }
