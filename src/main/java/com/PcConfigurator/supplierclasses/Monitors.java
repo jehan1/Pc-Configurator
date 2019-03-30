fd630dd816be72dab76d000696173c33b484d467
jehan <jehan.nilash@hotmail.com>
2019-03-30 02:46:05 +0000

    Update Monitors.java

--- src/main/java/com/PcConfigurator/supplierclasses/Monitors.java
+++ src/main/java/com/PcConfigurator/supplierclasses/Monitors.java
@@ -5,11 +5,10 @@
  */
 package com.PcConfigurator.supplierclasses;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import java.io.Serializable;
-import java.util.ArrayList;
+
 import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
@@ -18,8 +17,11 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 /**
- *
+ * This is the entity class for the Monitors database table
  * @author Jehan 
  */
 @Entity
@@ -33,33 +35,53 @@ public class Monitors extends Parts implements Serializable{
                         (name = "monitors_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "video_ports_id",
                     referencedColumnName = "id"))
-    public List<VideoPorts> videoPort;
+    private List<VideoPorts> videoPort;
     
     @NotNull
     private int hertz;
-    
-    public List<VideoPorts> getVideoPorts() {
-        return videoPort;
-    }
-
+ 
+    /**
+     * Empty constructor for the persistence framework
+     */
     public Monitors(){}
-    
+   
+    /**
+     * Initialise the video port object
+     * @param videoPorts list of video ports 
+     * @param hertz hertz value of the monitor
+     * @param id inherited from the super class
+     * @param name inherited from the superclass
+     */
     public Monitors(List<VideoPorts> videoPorts, int hertz, int id, String name) {
         super(id, name);
         this.videoPort = videoPorts;
         this.hertz = hertz;
     }
- 
-    public void setVideoPorts(List<VideoPorts> videoPorts) {
-        this.videoPort = videoPorts;
-    }
 
+    /**
+     * 
+     * @return the hertz value of the monitor  
+     */
     public int getHertz() {
         return hertz;
     }
 
+    /**
+     * sets the hertz value
+     * @param hertz hertz value of the monitor
+     */
     public void setHertz(int hertz) {
         this.hertz = hertz;
     }
+
+    /**
+     * 
+     * @return the set of video ports of the monitor
+     */
+    public List<VideoPorts> getVideoPort() {
+        return videoPort;
+    }
+
   
+    
 }
