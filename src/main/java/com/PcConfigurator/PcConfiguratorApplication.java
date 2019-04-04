f217d87151da9b01ffc32d599c5dfb0f40a725cd
jehan <jehan.nilash@hotmail.com>
2019-04-04 01:45:55 +0100

    fixed CorsMapping error

--- src/main/java/com/PcConfigurator/PcConfiguratorApplication.java
+++ src/main/java/com/PcConfigurator/PcConfiguratorApplication.java
@@ -2,12 +2,23 @@ package com.PcConfigurator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
+@EnableWebMvc
 @SpringBootApplication
-public class PcConfiguratorApplication {
+public class PcConfiguratorApplication extends WebMvcConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PcConfiguratorApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(PcConfiguratorApplication.class, args);
+    }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
+
